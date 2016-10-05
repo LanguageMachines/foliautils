634bd3c30925525996ebbd05281249ab58c9eb80
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-10-05 10:15:31 +0200

    implemented section-identifier as a FoLiA 'n' atribute. Fixed wrong section-path values

--- src/FoLiA-pm.cxx
+++ src/FoLiA-pm.cxx
@@ -1037,21 +1037,17 @@ void process_block1( Division *root, xmlNode *_block ){
       KWargs args;
       args["id"] = id;
       args["class"] = type;
-      Division *div1 = new Division( args, root->doc() );
-      root->append( div1 );
       string section_id = TiCC::getAttribute( block, "section-identifier" );
       if ( !section_id.empty() ){
-	args.clear();
-	args["subset"] = "section-identifier";
-	args["class"] = section_id;
-	Feature *f = new Feature( args );
-	div1->append( f );
+	args["n"] = section_id;
       }
+      Division *div1 = new Division( args, root->doc() );
+      root->append( div1 );
       string section_path =TiCC::getAttribute( block, "section-path" );
       if ( !section_path.empty() ){
 	args.clear();
 	args["subset"] = "section-path";
-	args["class"] = section_id;
+	args["class"] = section_path;
 	Feature *f = new Feature( args );
 	div1->append( f );
       }
