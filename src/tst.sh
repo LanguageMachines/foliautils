2da03533b30d19cd357be614dc8bc0508ce1c59c
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-02-26 12:34:30 +0100

    updated test script

--- src/tst.sh
+++ src/tst.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 
-cd ../tests
+if test -d ../tests;
+then
+    cd ../tests
 
-\rm -f *.diff
-\rm -f *.tmp
-\rm -f *.out*
+    \rm -f *.diff
+    \rm -f *.tmp
+    \rm -f *.out*
 
-for file in test2text testtxt testalto testcorrect \
-	    testhocr testidf testpage testlangcat teststats \
-	    testcollect testpm \
-	    testclean testwordtranslate
-do bash ./testone.sh $file
-done
+    for file in test2text testtxt testalto testcorrect \
+		testhocr testidf testpage testlangcat teststats \
+		testcollect testpm \
+		testclean testwordtranslate
+    do bash ./testone.sh $file
+    done
+else
+    exit 1
+fi
