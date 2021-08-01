39cf392aa8373cf919bb8dec0702ccbd3cf42779
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2021-08-01 18:56:08 +0200

    added more tests to make check

--- src/tst.sh
+++ src/tst.sh
@@ -8,8 +8,8 @@ then
     \rm -f *.tmp
     \rm -f *.out*
 
-    for file in test2text testtxt testalto testcorrect \
-		testhocr testidf testpage testlangcat teststats \
+    for file in test2text testtxt testalto testcorrect testcorbig \
+		testhocr testidf testpage testabby testlangcat teststats \
 		testpm testclean testwordtranslate
     do bash ./testone.sh $file
     done
